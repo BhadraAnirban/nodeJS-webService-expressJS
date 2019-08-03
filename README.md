8795ed2819d25f708d4c2cd9f864dbed64dac9dc
Anirban Bhadra <anirban.b@web.ilink2.com>
2019-08-03 15:09:42 +0530

    route params and query

--- README.md
+++ README.md
@@ -79,3 +79,29 @@ In case of mac: export PORT=2600
 node <node file name>: node expresshttp.js
 ```
 
+## Route Params, Query and optional params
+```
+app.get('/api/schools/:id', (req, res) => {
+    res.send(req.query);
+    // http://localhost:2600/api/schools/26?subject=nodeJS => {"subject":"nodeJS"}
+});
+
+app.get('/api/schools/:id/:department', (req, res) => {
+    res.send(req.params);
+    // http://localhost:2600/api/schools/26/22 => {"id":"26","department":"22"}
+});
+
+app.get('/api/schools/:id/department/:deptId', (req, res) => {
+    res.send(req.params); 
+    // http://localhost:2600/api/schools/26/department/22 => {"id":"26","deptId":"22"}
+});
+
+// below with ? mark, we have made departmant as Optional Parameter
+app.get('/api/library/:id/:department?', (req, res) => {
+    res.send(req.params);
+    // http://localhost:2600/api/library/26 => {"id":"26"}
+    // http://localhost:2600/api/library/26/22 => {"id":"26","department":"22"}
+});
+
+```
+
--- expresshttp.js
+++ expresshttp.js
@@ -9,6 +9,29 @@ app.get('/api/pmkj', (req, res) => {
     res.send('Pranabananda Maharaj');
 });
 
+app.get('/api/schools/:id', (req, res) => {
+    res.send(req.query);
+    // http://localhost:2600/api/schools/26?subject=nodeJS => {"subject":"nodeJS"}
+});
+
+app.get('/api/schools/:id/:department', (req, res) => {
+    res.send(req.params);
+    // http://localhost:2600/api/schools/26/22 => {"id":"26","department":"22"}
+});
+
+app.get('/api/schools/:id/department/:deptId', (req, res) => {
+    res.send(req.params); 
+    // http://localhost:2600/api/schools/26/department/22 => {"id":"26","deptId":"22"}
+});
+
+// below with ? mark, we have made departmant as Optional Parameter
+app.get('/api/library/:id/:department?', (req, res) => {
+    res.send(req.params);
+    // http://localhost:2600/api/library/26 => {"id":"26"}
+    // http://localhost:2600/api/library/26/22 => {"id":"26","department":"22"}
+});
+
+
 const port = process.env.PORT || 4100; // env is the environment object and PORT is the variable name.
 app.listen(port, () => {
     console.log(`server Started at ${port}...`);
